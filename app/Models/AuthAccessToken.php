28ac295eb9559583c99e36132bf1d1f2fdf2e77e
OPIYO OSCAR <148503309+OpiyoOscar01@users.noreply.github.com>
2025-05-10 16:17:13 +0300

    Fixed the Cookie bug

--- app/Models/AuthAccessToken.php
+++ app/Models/AuthAccessToken.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-use Faker\Provider\ar_EG\Person;
-use Illuminate\Database\Eloquent\Model;
+
 use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthAccessToken extends PersonalAccessToken
